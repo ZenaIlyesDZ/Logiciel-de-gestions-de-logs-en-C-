5d720632cd62fb855ad85e954f038712862787db
Ilyes Zenati <zenailyes93@gmail.com>
2026-01-17 14:16:54 +0000

    Etape 1.2

--- Gestion_logs.cpp
+++ Gestion_logs.cpp
@@ -1,3 +1,11 @@
+/*
+ * @file : Gestion_logs.cpp
+ * @brief summary of the role : Gestionnaire de logs en C++
+ * @author : Ilyes
+ * @date : 17/01/2026
+ * @details :
+ * This app allows you to manage logs efficiently, to store, retrieve, and analyze log data.
+ */
 #include <iostream>
 using namespace std;
 
