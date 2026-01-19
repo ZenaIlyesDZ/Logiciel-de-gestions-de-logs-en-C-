d9b83dbde0b5fcf7bb0695d6eae6251fafadd3a6
Ilyes Zenati <zenailyes93@gmail.com>
2026-01-19 04:57:06 +0000

    Interaction utilisateur et Traitement du cas d'erreur

--- Gestion_logs.cpp
+++ Gestion_logs.cpp
@@ -15,5 +15,14 @@ int main() {
     std::cout << "1 - Afficher log sudo" << std::endl;
     std::cout << "2 - Afficher et enregistrer log ssh" << std::endl;
     std::cout << "0 - Sortir du programme" << std::endl;
+    int nombreEntré; // Variable pour stocker le choix de l'utilisateur
+    if (!(std::cin >> nombreEntré)) { // On vérifie si l'entrée est un entier et non une lettre ou autre caractère
+        std::cout << "Entrée non valide. Veuillez entrer un chiffre." << std::endl; }
+    else if (nombreEntré != 0 && nombreEntré != 1 && nombreEntré != 2) { // On vérifie si l'entrée est parmi les options valides a savoir 0, 1 ou 2
+        std::cout << "Option non valide. Veuillez réessayer avec une option existante" << std::endl;
+    }
+    else { // Affiche le choix de l'utilisateur si la saisie est correcte
+        std::cout << "Vous avez choisi l'option : " << nombreEntré << std::endl; 
+    }
     return 0;
-}
+    }
