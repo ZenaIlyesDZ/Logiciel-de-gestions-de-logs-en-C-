718c5632ce442da8906644159ac2a16848ef736e
Ilyes Zenati <zenailyes93@gmail.com>
2026-01-19 12:37:43 +0000

    Première version de la documentation : fonction main

---
+++ Doc/Documentation_fonction.h
@@ -0,0 +1,9 @@
+/**
+ * @fn int main();
+ * @brief Fonction principale du programme de gestion des logs
+ * @author Ilyes
+ * @date Début Janvier 2026
+ * @return int : 0 Fonctionnement normal du programme
+ * @details
+ * Cette fonction affiche un menu permettant à l'utilisateur de choisir entre différentes options de gestion des logs
+ */
