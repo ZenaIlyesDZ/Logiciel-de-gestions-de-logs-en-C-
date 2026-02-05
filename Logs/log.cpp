6bdfaeccf482a8bc476ec76967c6912dff786b9c
Ilyes Zenati <zenailyes93@gmail.com>
2026-02-05 11:42:19 +0000

    sudoLog only show sudo Logs

--- Logs/log.cpp
+++ Logs/log.cpp
@@ -16,11 +16,14 @@ Ce fichier contient les fonctions pour afficher les logs, comme les logs sudo, l
 
 int sudoLog() { // Variable pour stocker le choix de l'utilisateur pour les logs sudo
     if (nombreEntré == 1) { // Si l'utilisateur choisit 1, on affiche les logs sudo
-    std::ifstream file("/var/log/auth.log"); // Ouvre le fichier de logs sudo
-    std::string line;
-    if (file.is_open()) {
-        while (std::getline(file, line)) {
-        std::cout << line << std::endl;
+    std::ifstream file("/var/log/auth.log"); // Ouvre le fichier de tests
+    std::string line; // Variable pour stocker chaque ligne lue du fichier
+    
+    if (file.is_open()) { // Vérifie si le fichier est ouvert correctement
+        while (std::getline(file, line)) { // Lit chaque ligne du fichier
+            if (line.find("sudo") != std::string::npos) { // Si la ligne contient "sudo", on l'affiche
+                std::cout << line << std::endl;
+            } 
         }
     file.close();
     } else {
