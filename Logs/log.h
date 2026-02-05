ce66cac7760247f2376db5167ecc319768b35ca7
Ilyes Zenati <zenailyes93@gmail.com>
2026-02-05 09:07:09 +0000

    Creation of log.h This file contain the prototype of the functions present in log.cpp

---
+++ Logs/log.h
@@ -0,0 +1,10 @@
+/*
+@file : log.h
+@brief summary of the role : Fichier avec la déclaration des fonctions pour afficher les logs
+@author : Ilyes
+@date : 05/02/2026
+@details :
+Ce fichier contient les déclarations des fonctions pour afficher les logs, comme les logs sudo, logs ssh.
+*/
+
+int sudoLog();
