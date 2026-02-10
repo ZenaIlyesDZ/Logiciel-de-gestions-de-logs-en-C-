fa3408899e31e08b580833a5fe0be8c77de01ebb
Ilyes Zenati <zenailyes93@gmail.com>
2026-02-10 10:41:10 +0000

    Add 2 structures

--- Logs/log.h
+++ Logs/log.h
@@ -4,10 +4,35 @@
 @author : Ilyes
 @date : 05/02/2026
 @details :
-Ce fichier contient les déclarations des fonctions pour afficher les logs, comme les logs sudo, logs ssh.
+Ce fichier contient les déclarations des fonctions pour afficher les logs, comme les logs sudo ou logs ssh.
 */
+
+#ifndef LOG_H
+#define LOG_H
+#include <string>
+#include <unistd.h>
+
+struct sshDateTime
+{ 
+    std::string day;
+    std::string hour; 
+    std::string minute; 
+    std::string second; 
+};
+
+struct SSHLogging
+{
+    std::string hostname;
+    std::string username; 
+    std::string sshUser; 
+    std::string sshHost;
+    int sshPort;
+    sshDateTime Date;
+};
 
 int sudoLog();
 int sshLog();
 void hostName(SSHLogging &sshLog);
 void userName(SSHLogging &sshLog);
+
+#endif
